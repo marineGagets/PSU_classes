843c5138510f89a6290f3a075e438d2d2bcc682d
Larry Bilodeau <crebsmaynard@gmail.com>
2024-08-02 14:29:36 -0700

    initial project set up

---
+++ cs445 program2/code/Gaussian_Naive_Bayes_exercise.py
@@ -0,0 +1,13 @@
+'''
+Machine Language Programming assignment 2
+see the assignment description in the README.md file
+'''
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the raw data
+rawdata = pd.read_csv('..\datasets\spambase.data', header=None)
+
+
